1c787badab9ea9822713aeb25d5d9646a0a6a2b1
theheroproject <58100691+theheroproject@users.noreply.github.com>
2021-11-11 00:14:00 +0900

    20211111

---
+++ class-note/5_operator.ts
@@ -0,0 +1,13 @@
+function logMessage(value: any){
+    console.log(value);
+}
+logMessage('hello');
+logMessage(100);
+
+
+function logMessages(value: string|number){
+    console.log(value);
+}
+
+logMessages('hello');
+logMessages(100);
